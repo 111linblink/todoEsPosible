84e32e2a8ecba639d7d70f93d8475b9d5b46ceed
111linblink <cabrera.karen.vidal.8@gmail.com>
2025-06-05 19:22:47 -0600

    contenidoooo en feature-content

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Header from './components/feature-header';
+import Content from './components/feature-content';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header/>
+    <Content/>
   </React.StrictMode>
 );
 
